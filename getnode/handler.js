78999e1b32288ed8c3c2374106fcb0f2519b66c8
ben <benard-idemudia.obhade@stud.uni-bamberg.de>
2018-10-02 16:56:57 +0200

    console log

--- getnode/handler.js
+++ getnode/handler.js
@@ -10,26 +10,28 @@ module.exports = (context, callback) => {
     request.get('http://gateway:8080/function/getnextjob')
         .pipe(request.post('http://gateway:8080/function/getxml', (error, response, data) => {
 
-            //parse job to json object
-            const jobxml = JSON.parse(data);
-            let  job = jobxml.job;
-            let  xml = jobxml.finalJob;
+            // //parse job to json object
+            // const jobxml = JSON.parse(data);
+            // let  job = jobxml.job;
+            // let  xml = jobxml.finalJob;
 
-            // assign job to the correct processpath and position
-            let position = job.processposition;
-            let processName = job.processname;
+            // // assign job to the correct processpath and position
+            // let position = job.processposition;
+            // let processName = job.processname;
 
-            //set the node
-            const node0 = xml.MAIN._attributes;
+            // //set the node
+            // const node0 = xml.MAIN._attributes;
 
-            if (position === "" ) {
-                position = node0.BEGIN;
-            }
-            job.processposition = position;
+            // if (position === "" ) {
+            //     position = node0.BEGIN;
+            // }
+            // job.processposition = position;
 
-            const procName = job.processpath.split(":")[0];
-            job.processpath = ` ${procName}: ${position}`;
-            callback(null, {job, xml} );
+            // const procName = job.processpath.split(":")[0];
+            // job.processpath = ` ${procName}: ${position}`;
+
+            console.log('here is my data', data);
+            callback(null, data );
 
         }));
 };
